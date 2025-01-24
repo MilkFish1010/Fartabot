a24d9cba063ac0a7e2f81344d30cc28b4d35b421
MilkFish1010 <hoby.josol1010@gmail.com>
2025-01-24 21:17:37 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -678,9 +678,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "",
+    "c_user": "61572575755731",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": ""
+    "xs": "22%3AcwpnGlF9bvussA%3A2%3A1737721679%3A-1%3A-1%3A%3AAcWIXnV6vwRCFjRb6VhdQh_JXG2SX1rWBUgdPrWGew"
 }
 
 
